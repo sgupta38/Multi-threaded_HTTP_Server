ca48e0b5981ce74a77a923f6d1a082e992418436
Sonu Gupta <sonugupta4636@gmail.com>
2018-09-05 01:02:09 -0400

    Added code for parsing'HTTP request' using regular expression.
     - Inbuilt 'regular expression' library is used for parsing.
     - Todo: Add code to check presence of 'www' directory and look resources.

--- HTTPServer.py
+++ HTTPServer.py
@@ -2,12 +2,17 @@
 # @Date: 9/4/18
 # @Purpose: This file serves as a HTTP Server which.
 #
+#  Client request: URL = "http://remote02.cs.binghamton.edu:47590/bar.html"
 
 import sys
+import re
 import socket
 
 host = '127.0.0.1'
 port = 65431
+
+#@note: Follwoing is a regular expression to parse the HTTP request and get the resource name.
+request_pattern = r'(?P<Header>http://)(?P<Host_info>[\w|\.|\d|:|/|~|\- ]*)/(?P<Resource>[\w|\-|\.|\d]*)'
 
 # Response will be standard HTTP Response header.
 # @todo: Need to change content-type, since we are sending file.
@@ -15,6 +20,12 @@ response = """
 Status: 200 OK
 Content-type: text/html
 """
+
+# @note: Function used to parse the HTTP request, and get the resource which needs to be looked up in 'www' directory.
+def parseRequest(request):
+    pattern = re.compile(request_pattern)
+    match = pattern.match(request)
+    return match.group('Resource')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 sock.bind((host, port))
@@ -34,6 +45,8 @@ while True:
         print("Request received is: ", data.decode('ascii'))
         # parse request shall go here, and send response back to client
         # todo: parsing , logic for 'www' directory.
+        rs = parseRequest(data.decode('ascii'))
+        print('Need to check Resource: ', rs)
         conn.sendall(response.encode('ascii'))
         conn.close()
     break
