c3f7a4b382aa95266cfa8d5f0d809417b46e55e6
Sonu Gupta <sonugupta4636@gmail.com>
2018-09-07 00:01:48 -0400

    Updated code to download files from 'wget' and 'browser'.
     - Added code for checking directory 'www' and respective resources.
     - Changed regular expression to parse the HTTP request
     - Wrote a function to generate 'HTTP Response'
     - Currently, 'HTTPServer' is single threaded and can handle many requests one by one.
    
      Todo:
     - Add support for multithreading.
     - Modularize codebase.
     - code cleaning.
     - Update documentation.
     - Test locally and at remote server.

--- HTTPServer.py
+++ HTTPServer.py
@@ -1,25 +1,45 @@
 # @Author: Sonu Gupta
 # @Date: 9/4/18
-# @Purpose: This file serves as a HTTP Server which.
+# @Purpose: This file serves as a HTTP Server.
 #
-#  Client request: URL = "http://remote02.cs.binghamton.edu:47590/bar.html"
 
+import os
 import sys
 import re
 import socket
+import time
+from wsgiref.handlers import format_date_time
+from mimetypes import MimeTypes
 
 host = '127.0.0.1'
 port = 65431
 
-#@note: Follwoing is a regular expression to parse the HTTP request and get the resource name.
-request_pattern = r'(?P<Header>http://)(?P<Host_info>[\w|\.|\d|:|/|~|\- ]*)/(?P<Resource>[\w|\-|\.|\d]*)'
+request_pattern = r'GET (?P<Resource>(.*)) HTTP/1.1'
 
-# Response will be standard HTTP Response header.
-# @todo: Need to change content-type, since we are sending file.
-response = """
-Status: 200 OK
-Content-type: text/html
-"""
+# todo: modularize for error 404
+def createHTTP_Response_header(resource_name):
+    HTTP_status = 'HTTP/1.1 200 OK\r\n'
+    HTTP_connection = 'Connection: Keep-Alive\r\n'   # Keeps connection alive and avoids "incomplete downloa" issue.
+    date = format_date_time(time.time())
+    HTTP_date = 'Date: ' + date + '\r\n'
+    server = 'cs.binghamton.uni'
+    HTTP_server = 'Server: ' + server + '\r\n'
+    timestamp = os.path.getmtime(resource_name)
+    last_modified = format_date_time(timestamp)
+    HTTP_lastModified = 'Last-Modified: ' + last_modified + '\r\n'
+    length = os.path.getsize(resource_name)
+    HTTP_length = 'Content-Length: ' + str(length) + '\r\n'
+    mime = MimeTypes()
+    mime_type = mime.guess_type(resource_name)
+    type = mime_type[0]
+    if type is None:
+        HTTP_type = 'Content-Type: None\r\n'
+    else:
+        HTTP_type = 'Content-Type: ' + type + '\r\n'
+    HTTP_Response = HTTP_status + HTTP_date + HTTP_server + HTTP_lastModified + HTTP_length + HTTP_type + HTTP_connection + '\r\n'
+
+    return HTTP_Response
+
 
 # @note: Function used to parse the HTTP request, and get the resource which needs to be looked up in 'www' directory.
 def parseRequest(request):
@@ -30,25 +50,42 @@ def parseRequest(request):
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 sock.bind((host, port))
 sock.listen()
-print("\nHTTP Server is Listening at \n")
-print("Host: ", host)
-print("Port: ", port)
+print('\nHTTP Server is Listening at \n')
+print('Host: ', host)
+print('Port: ', port)
 
-conn, addr = sock.accept()
-
-print('\nConnected to', addr)
 
 while True:
+    conn, addr = sock.accept()
+    print('\nConnected to', addr)
     # receive request here
-    data = conn.recv(1024);
-    if data is not None:
-        print("Request received is: ", data.decode('ascii'))
-        # parse request shall go here, and send response back to client
-        # todo: parsing , logic for 'www' directory.
-        rs = parseRequest(data.decode('ascii'))
-        print('Need to check Resource: ', rs)
-        conn.sendall(response.encode('ascii'))
-        conn.close()
-    break
+    req = conn.recv(1024);
+    if req is not None:
+        print('Request received is: ', req.decode('ascii'))
+        rs = parseRequest(req.decode('ascii'))
+        isDir = os.path.isdir('www');
+        if isDir:
+            ## check for presence of resource file. other wise return error 404.
+            print('Need to check Resource: ', rs)
+            resource = 'www' + rs
+            isResource = os.path.exists(resource)
+            if isResource:
+                http_response_header = createHTTP_Response_header(resource)
+                conn.sendall(http_response_header.encode('ascii'))
+                fsendfile = open(resource, 'rb')
+                data = fsendfile.read(1024)
+                #data = http_response_header.encode('ascii') + data
+                while data:
+                    conn.sendall(data)
+                    data = fsendfile.read(1024)
 
-print('client request is served')
+                fsendfile.close()
+                conn.close() # after sending close socket.
+                print('Send completed')
+                print('Client request is served')
+
+        else:
+            print('404 error not found')
+            conn.close() # after sending close socket.
+
+
